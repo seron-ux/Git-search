16a3fb32c97e9154f4fb63b47f87d8f45c34d765
Seron-ux <abyseronkirui@gmail.com>
2021-02-07 15:07:09 +0300

    install bootstrap link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-// export const environment = {
-//   production: true,
-//   apiKey: "9bbe14a39239505e886d5386f7931cd081941269"
-// };
+export const environment = {
+  production: true,
+  apiKey: "9bbe14a39239505e886d5386f7931cd081941269"
+};
