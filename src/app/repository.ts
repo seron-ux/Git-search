c183201efe5ebaf5b984aee0f421eed7ad2525b8
Seron-ux <abyseronkirui@gmail.com>
2021-02-07 14:42:16 +0300

    search class github

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,10 +1,10 @@
-// export class Repository {
-//     constructor(
-//        public name:any,
-//        public description:any,
-//        public created_at:any,
-//        public html_url:any,
-//     ){
+export class Repository {
+    constructor(
+       public name:any,
+       public description:any,
+       public created_at:any,
+       public html_url:any,
+    ){
 
-//     }
-// }
+    }
+}
